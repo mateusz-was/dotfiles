4cbca8a9e7d03b2ad4e19742d19242abb794020e
Mateusz Was <m.was@op.pl>
2017-12-01 23:26:15 -0500

    fixes

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@ chmod +x install/* system/sudoers install.sh
 # Verify that your user will be able to do all the sudo actions
 ./system/sudoers
 # Ensure basic packages exists
-./requirements.sh
+./install/requirements.sh
 
 # Install packages
 ./install/git.sh
 ./install/zsh.sh
-zsh
-source ~/.zshrc
--- install/requirements.sh
+++ install/requirements.sh
@@ -2,11 +2,11 @@ echo "### Install Git: Start ###"
 if (cat /etc/os-release | grep -qi ubuntu); then
   set -x
   sudo apt update
-  sudo apt get -y curl git python-pip
+  sudo apt install -y curl git python-pip
   set +x
 fi
 if (cat /etc/os-release | grep -qi centos); then
   sudo yum update
-  sudo yum install -y git python-pip
+  sudo yum install -y curl git python-pip
 fi
 echo "### Install Git: End ###"
