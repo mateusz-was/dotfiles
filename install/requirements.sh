e7e2c54478aa3303ee6893ba702d4b517d5db524
Mateusz Was <mateusz.was@tomtom.com>
2018-03-30 14:45:59 -0400

    Simplify base packages list

--- install/requirements.sh
+++ install/requirements.sh
@@ -1,12 +1,13 @@
 echo "\033[0;32m### Install git, curl, pip: Start ###\033[0m"
+
+packages=("curl" "git" "python-pip")
+
 if (cat /etc/os-release | grep -qi ubuntu); then
-  set -x
   sudo apt update
-  sudo apt install -y curl git python-pip
-  set +x
+  sudo apt install -y $packages
 fi
 if (cat /etc/os-release | grep -qi centos); then
   sudo yum update
-  sudo yum install -y curl git python-pip
+  sudo yum install -y $packages
 fi
 echo "\033[0;32m### Install  git, curl, pip: End ###\033[0m"
