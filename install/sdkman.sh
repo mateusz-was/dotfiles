aab4160873453bb78cd0f13f9ff8fff0a70540dd
Mateusz Was <mateusz.was@tomtom.com>
2018-03-30 14:45:04 -0400

    Switch sdkman to use zsh

--- install/sdkman.sh
+++ install/sdkman.sh
@@ -1,5 +1,4 @@
 echo "\033[0;32m### Install SDKman: Start ###\033[0m"
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
+curl -s "https://get.sdkman.io" | zsh
+zsh -c ". $HOME/.sdkman/bin/sdkman-init.sh && sdk version"
 echo "\033[0;32m### Install SDKman: End ###\033[0m"
