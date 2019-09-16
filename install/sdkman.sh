39f850a5a2deea53b2881f75d3bf6ff78ddb4ef6
Mateusz Was <mateusz.was@tomtom.com>
2019-09-16 14:11:21 +0200

    install mvn, gradle, groovy

--- install/sdkman.sh
+++ install/sdkman.sh
@@ -1,4 +1,10 @@
 echo "\033[0;32m### Install SDKman: Start ###\033[0m"
+
 curl -s "https://get.sdkman.io" | zsh
 zsh -c ". $HOME/.sdkman/bin/sdkman-init.sh && sdk version"
+
+sdk i maven
+sdk i gradle
+sdk i groovy
+
 echo "\033[0;32m### Install SDKman: End ###\033[0m"
