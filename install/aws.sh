efedca319c4b3cd446a27617badb84d0116e2c77
Mateusz Was <mateusz.was@tomtom.com>
2019-09-25 11:38:42 +0200

    fix pip install

--- install/aws.sh
+++ install/aws.sh
@@ -1,3 +1,3 @@
 echo "\033[0;32m### Install aws-cli: Start ###\033[0m"
-sudo pip install awscli --upgrade --user
+pip install awscli --upgrade --user
 echo "\033[0;32m### Install aws-cli: End ###\033[0m"
